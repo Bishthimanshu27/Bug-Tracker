3c370525fa7a728c1f566ff17a67f082f3b6e8f9
Himanshu Bisht <Himanshubisht227@gmail.com>
2018-10-05 00:03:43 -0500

    first commit

---
+++ Bug tracker/Migrations/Configuration.cs
@@ -0,0 +1,64 @@
+namespace Bug_tracker.Migrations
+{
+    using Bug_tracker.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Bug_tracker.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "BugTracker.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Bug_tracker.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+            if (!context.Roles.Any(r => r.Name == "Project Manager"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Project Manager" });
+            }
+            if (!context.Roles.Any(r => r.Name == "Developer"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Developer" });
+            }
+            if (!context.Roles.Any(r => r.Name == "Submitter"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Submitter" });
+            }
+            ApplicationUser adminUser;
+            if (!context.Users.Any(item => item.UserName == "admin@admin.com"))
+            {
+                adminUser = new ApplicationUser();
+                adminUser.UserName = "admin@admin.com";
+                adminUser.Email = "admin@admin.com";
+                adminUser.LastName = "Admin";
+                adminUser.FirstName = "Himanshu";
+                adminUser.DisplayName = "Himanshu Bisht";
+                userManager.Create(adminUser, "Password-1");
+            }
+            else
+            {
+                adminUser = context.Users.FirstOrDefault(item => item.UserName == "admin@admin.com");
+            }
+            if (!userManager.IsInRole(adminUser.Id, "Admin"))
+            {
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
+        }
+    }
+}
---
+++ Bug tracker/Models/IdentityModels.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Bug_tracker.Models.Classes;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Bug_tracker.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string DisplayName { get; set; }
+
+        public ApplicationUser()
+        {
+            Projects = new HashSet<Project>();
+        }
+
+        public virtual ICollection<Project> Projects { get; set; }
+       
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<Bug_tracker.Models.Classes.Project> Projects { get; set; }
+
+    }
+}
